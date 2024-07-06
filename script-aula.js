46155beb5fdac854f84c121bea67385f09731662
Messias Fernandes <fr.messias27@gmail.com>
2024-07-06 17:58:04 -0300

    Basic Java Script

---
+++ script-aula.js
@@ -0,0 +1,31 @@
+const animais = document.getElementById('animais');
+console.log(animais)
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection[1])
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl)
+
+const linksInternos = document.querySelector('[href^="#"]')
+console.log(linksInternos.href)
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg)
+
+const gridSectionHTML = document.getElementsByClassName('grid-section')
+const gridSectionNode = document.querySelectorAll('.grid-section')
+
+
+
+primeiraUl.classList.add('grid-section')
+
+gridSectionNode.forEach(function(item, index){
+  console.log(item)
+})
+
+const arrayGrid = Array.from(gridSectionHTML)
+console.log(arrayGrid)
---
+++ script.js
@@ -0,0 +1,14 @@
+const imagens = document.querySelectorAll('img')
+console.log(imagens)
+
+const img2 = document.querySelectorAll('img[src^="./img/imagem"]')
+console.log(img2)
+
+const linksInterno = document.querySelectorAll('[href^="#"]')
+console.log(linksInterno)
+
+const h2Animais = document.querySelector('.animais-descricao h2')
+console.log(h2Animais)
+
+const direitos = document.querySelector('footer p')
+console.log(direitos)
