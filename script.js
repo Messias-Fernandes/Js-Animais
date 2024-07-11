b0b0f5900d969d7ea67e2af8cc590778fd1bdf5a
Messias Fernandes <fr.messias27@gmail.com>
2024-07-10 23:54:47 -0300

    js events

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 // eventos 
-const img = document.querySelectorAll('img')
+const imgs = document.querySelectorAll('img')
 
-function imgSrc (event){
-  const src = event.currentTarget.get
+function handleImg (event) {
+  console.log(event.target )
 }
+
+imgs.forEach((img) => {
+  img.addEventListener('click', handleImg)
+})
+
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+
+function handleLink(event) {
+  event.preventDefault();
+  event.currentTarget.classList.add('ativo')
+  linksInternos.forEach((link) => {
+    link.classList.remove('ativo')
+  });
+  event.currentTarget.classList.add('ativo')
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', handleLink)
+})
+
+function handleElement(event){
+  event.currentTarget.remove()
+}
+
+const todos = document.querySelectorAll('body *')
+todos.forEach((elemento) => {
+  elemento.addEventListener('click', handleElement)
+})
+
+
+function handleClickT(event){
+  console.log(event.key)
+  if(event.key === 't'){
+    console.log('clicou t')
+  }
+}
+
+window.addEventListener('keydown', handleClickT)
